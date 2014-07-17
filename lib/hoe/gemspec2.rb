d1451ace0b27b8fad41b9e147ed5613543896051
Austin Ziegler <austin@zieglers.ca>
2014-07-16 22:51:10 -0400

    Removing cert_chain and signing_key from gemspec
    If you use gem signing, those lines should not be committed to the
    gemspec in the repo for use in Bundler. They should be used when
    generating the spec for bundling, but this task is only used for
    recording the gemspec for use with Bundler.
    
    This change stops writing those lines.
    
    An additional task has been added to generate the full gemspec,
    including lines related to gem signing.

--- lib/hoe/gemspec2.rb
+++ lib/hoe/gemspec2.rb
@@ -1,5 +1,5 @@
 module Hoe::Gemspec2
-  VERSION = '1.1.0'
+  VERSION = '1.2.0'
 
   def define_gemspec2_tasks
     gemspec = spec.name + '.gemspec'
@@ -11,11 +11,28 @@ module Hoe::Gemspec2
     end
 
     file gemspec => %w[clobber Manifest.txt] + spec.files do
-      open(gemspec, 'w') { |f| f.write(spec.to_ruby) }
+      open(gemspec, 'w') { |f|
+        spec2 = YAML.load(YAML.dump(spec))
+
+        unless @include_all
+          [ :signing_key, :cert_chain ].each { |name|
+            spec2.send("#{name}=".to_sym, spec2.default_value(name))
+          }
+        end
+        f.write(spec2.to_ruby)
+      }
     end
 
-    desc "Regenerate #{gemspec}"
+    desc "Regenerate #{gemspec} excluding signing keys"
     task :gemspec => gemspec
+
+    namespace :gemspec do
+      desc "Regenerate #{gemspec} with all keys"
+      task :full do
+        @include_all = true
+        Rake::Task['gemspec'].invoke
+      end
+    end
 
     task :default => gemspec
   end
